b05e7ac1510b128c84153a18ac1e9f5f1ad98539
Austin <paau@odoo.com>
2025-05-12 16:28:25 -0700

    finished task

---
+++ douglas_doc_layout/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Doug Custom Doc Layout",
+    "summary": "[paau]: Douglas King Seeds: Customizing document layout",
+    "category": "Training/Doc Layout",
+    "version": "18.0.0.0.1",
+    "author": "Austin Paskerian",
+    "license": "OPL-1",
+    "application": "False",
+    "data": ["views/report_invoice_document.xml"],
+    "depends": [],
+}
