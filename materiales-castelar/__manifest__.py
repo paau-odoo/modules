1f83bd8dccec72f36c7dd9c4095cb25fd66d3864
Austin <paau@odoo.com>
2025-05-13 10:46:35 -0700

    [ADD] task requirements

---
+++ materiales-castelar/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "materiales-castelar-warehouse",
+    "summary": "[paau]: Materiales Castelar: Warehouse shouldn't receive more than ordered quantity",
+    "category": "Training/MCW",
+    "version": "18.0.0.0.1",
+    "author": "Austin Paskerian",
+    "license": "OPL-1",
+    "application": "False",
+    "data": [],
+    "depends": ["stock", "purchase"],
+}
---
+++ materiales-castelar/models/stock_move.py
@@ -0,0 +1,20 @@
+from odoo import api, fields, models
+from odoo.exceptions import UserError
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    @api.onchange("quantity")
+    def _onchange_quantity_check(self):
+
+        # Only do this on receipts
+        if self.picking_id.picking_type_code == "incoming":
+            # If the q is larger than demand, show the warning
+            if self.quantity > self.product_uom_qty:
+                return {
+                    "warning": {
+                        "title": "Warning",
+                        "message": "You can't receive more than the ordered quantity. Please, enter another quantity",
+                    }
+                }
