cbbff5496f25e5300b987371006dc4d3b3f9f9e3
Austin Paskerian <paau@odoo.com>
2025-05-15 10:46:53 -0700

    [FIX] Update __manifest__.py to version 15

--- tvu-cron/__manifest__.py
+++ tvu-cron/__manifest__.py
@@ -2,7 +2,7 @@
     "name": "tvu-cron",
     "summary": "[paau]: TVU Networks: Auto-Cancel Expired Quotations",
     "category": "Training/TVU",
-    "version": "18.0.0.0.1",
+    "version": "15.0.0.0.1",
     "author": "Austin Paskerian",
     "license": "OPL-1",
     "application": "False",
