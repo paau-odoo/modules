9769146aa057f4e85822160066d65f21ff8e8423
Austin <paau@odoo.com>
2025-05-15 16:11:48 -0700

    [ADD] task requirements

---
+++ msp-strat/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "msp-strat",
+    "summary": "[paau]: Media Sales Plus : Strategic Field Custom Action",
+    "category": "Training/MSP",
+    "version": "18.0.0.0.1",
+    "author": "Austin Paskerian",
+    "license": "OPL-1",
+    "application": "False",
+    "data": ["views/res_partner.xml", "data/cron.xml"],
+    "depends": ["contacts"],
+}
