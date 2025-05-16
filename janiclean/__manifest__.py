89ed95575e06833021cce798ea7a35a846f6b9f6
Austin <paau@odoo.com>
2025-05-16 12:06:15 -0700

    [ADD] task requirements

---
+++ janiclean/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "janiclean",
+    "summary": "[paau]: JaniClean: Automation",
+    "category": "Training/JANI",
+    "version": "17.0.0.0.1",
+    "author": "Austin Paskerian",
+    "license": "OPL-1",
+    "application": "False",
+    "depends": ["mail", "contacts", "hr", "sale_management"],
+    "data": [
+        "data/email_birthday.xml",
+        "data/cron_birthday.xml",
+    ],
+}
