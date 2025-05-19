5485bac319c3c7966c4b37a8ce4de3eeebe89e79
Austin <paau@odoo.com>
2025-05-19 12:36:02 -0700

    [ADD] task requirements

---
+++ wdso/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "wdso",
+    "summary": "[paau]: Wiexe Digital : SO list view computed field",
+    "category": "Training/wdso",
+    "version": "17.0.0.0.1",
+    "author": "Austin Paskerian",
+    "license": "OPL-1",
+    "application": "False",
+    "data": ["views/sale_order_list_views.xml"],
+    "depends": ["sale_management", "purchase", "stock_account"],
+}
