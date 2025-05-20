dae4bcbe4c10baf1144a9f5222e9ca921fe7d5f6
Austin <paau@odoo.com>
2025-05-20 10:42:52 -0700

    [ADD] task requirements

---
+++ tan-invoice/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "tan-invoice",
+    "summary": "[paau]: Tangent Systems LLC: Invoice Created from Task",
+    "category": "Training/Tan",
+    "version": "16.0.0.0.1",
+    "author": "Austin Paskerian",
+    "license": "OPL-1",
+    "application": "False",
+    "data": ["views/project_task_form.xml"],
+    "depends": ["sale_management", "project"],
+}
---
+++ tan-invoice/models/project_task.py
@@ -0,0 +1,39 @@
+from odoo import models, fields, api, Command
+from datetime import timedelta
+
+
+class Task(models.Model):
+    _inherit = "project.task"
+
+    # New fields
+    delivery_date = fields.Date(string="Delivery date")
+    invoice_number = fields.Many2one(comodel_name="account.move", string="Invoice")
+
+    @api.onchange("delivery_date")
+    def _onchange_delivery_date(self):
+        # Trigger creating an invoice for billable tasks if a delivery date has been set, AND there is no prior associated invoice
+        if self.sale_line_id and self.delivery_date and not self.invoice_number:
+            # Create the invoice
+            inv = self.env["account.move"].create(
+                {
+                    "state": "draft",
+                    "move_type": "out_invoice",
+                    "partner_id": self.partner_id.id,
+                    "invoice_date": self.delivery_date,
+                    "line_ids": [
+                        Command.create(
+                            {
+                                "product_id": self.sale_line_id.product_id.id,
+                                "price_unit": self.sale_line_id.price_unit,
+                                "quantity": 1,  # They said always q = 1, otherwise would come from sale line id
+                            }
+                        )
+                    ],
+                    "invoice_date_due": self.delivery_date
+                    + timedelta(days=7),  # Make the due date 7 days out or something
+                }
+            )
+            # Increment quantity delivered on SO
+            self.sale_line_id.qty_delivered += 1
+            # Attach invoice to this task
+            self.invoice_number = inv
